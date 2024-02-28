04dc5975dc12033470f00835be5d57406ba389c3
taicheee05 <kyokui150101@gmail.com>
2024-02-28 21:51:18 +0900

    spreadsheet_reflect.py update

--- spreadsheet_reflect.py
+++ spreadsheet_reflect.py
@@ -2,13 +2,18 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 # スプレッドシートへの接続設定
-def spreadsheet_reflect()
+def spreadsheet_reflect(calculate):
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('path/to/your/service-account.json', scope) #jsonが配置されている場所を記入する
     client = gspread.authorize(creds)
     # スプレッドシートを開く
     spreadsheet = client.open('your_spreadsheet_name')  # スプレッドシート名を指定
     worksheet = spreadsheet.sheet1  # 最初のワークシートを選択
+    #回答を列に追加していく。
+    cells=[]
+    for answer in calculate:
+        cell=answer[]
+
 
 # 回答を提出するボタンが押されたとき
     if st.button("回答を提出する"):
