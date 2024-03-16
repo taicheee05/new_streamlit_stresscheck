b4111e74a0c77134921a3e55407f09556f75e5c7
taicheee05 <kyokui150101@gmail.com>
2024-03-16 12:58:16 +0900

    更新

--- high_stress_check.py
+++ high_stress_check.py
@@ -1,39 +1,39 @@
 
 #領域Bの合計点数が77点以上
 #領域AとCの合算が76点以上、かつ、領域Bの合計が63点以上
-#領域Bの点数
-def b_stress_score(calculate):
+#領域aの点数
+def a_stress_score(calculate):
     # 初期スコア
-    b_stress_score = 0
+    a_stress_score = 0
     # 5から引く必要があるインデックスのリスト
     subtract_indices = [1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 15]
     # 合計計算
     for i in range(1, 18):  # 1から17まで (calculate[1] から calculate[17] まで)
         if i in subtract_indices:
-            b_stress_score += (5 - calculate[i])
+            a_stress_score += (5 - calculate[i])
         else:
-            b_stress_score += calculate[i]
-    return b_stress_score
-#領域Aの点数
-def a_stress_score(calculate):
+            a_stress_score += calculate[i]
+    return a_stress_score
+#領域bの点数
+def b_stress_score(calculate):
     # 初期スコア
-    a_stress_score = 0
+    b_stress_score = 0
     # 5から引く必要があるインデックスのリスト
     subtract_indices = [18, 19, 20]
     # 合計計算
     for i in range(18, 47):  
         if i in subtract_indices:
-            a_stress_score += (5 - calculate[i])
+            b_stress_score += (5 - calculate[i])
         else:
-            a_stress_score += calculate[i]
-    return a_stress_score
+            b_stress_score += calculate[i]
+    return b_stress_score
 
 #領域Cの点数
 def c_stress_score(calculate):
     # 初期スコア
     c_stress_score = 0
     # 合計計算
-    for i in range(47, 55):  
+    for i in range(47, 56):  
         c_stress_score += calculate[i]
     return c_stress_score
 
