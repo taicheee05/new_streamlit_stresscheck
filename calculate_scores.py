187d0d3da5ea78bcfee361c8dbef41512f6458f1
taicheee05 <kyokui150101@gmail.com>
2024-03-16 20:36:55 +0900

    更新

--- calculate_scores.py
+++ calculate_scores.py
@@ -19,7 +19,6 @@ def calculate_score(answers, questions):
 
 def calculate_soten_score(calculate, outputs):
     results={}
-    syakudo_value={}
     score = 0
     # ユーザーの回答と質問データを照合してスコアを計算
     for output in outputs:
@@ -106,60 +105,60 @@ def syakudo_values_test(calculate, outputs):
         o_id = output['番号']
         if o_id==1:
             score = 15 - (calculate[1] + calculate[2] + calculate[3])
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==2:
             score = 15 - (calculate[4] + calculate[5] + calculate[6])
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==3:
             score = 5 - calculate[7]
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==4:
             score = 10 -(calculate[12] + calculate[13]) +calculate[14]
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==5:
             score = 5- calculate[15]
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==6:
             score = 15 - (calculate[8] + calculate[9] + calculate[10])
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==7:
             score = calculate[11]
-            syakudo_value[output[o_id]] = score                
+            syakudo_value[output['番号']] = score                
         elif o_id==8:
             score = 5 - calculate[16]
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==9:
             score =5 - calculate[17]
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==10:
             score =calculate[18]+calculate[19]+calculate[20]
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==11:
             score=calculate[21]+calculate[22]+calculate[23]
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==12:
             score=calculate[24]+calculate[25]+calculate[26]
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==13:
             score=calculate[27]+calculate[28]+calculate[29]
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==14:
             score=calculate[30]+calculate[31]+calculate[32]+calculate[33]+calculate[34]+calculate[35]
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==15:
             score=calculate[36]+calculate[37]+calculate[38]+calculate[39]+calculate[40]+calculate[41]+calculate[42]+calculate[43]+calculate[44]+calculate[45]+calculate[46]
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==16:
             score=15-(calculate[47]+calculate[50]+calculate[53])
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==17:
             score=15-(calculate[48]+calculate[51]+calculate[54])
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==18:
             score=15-(calculate[49]+calculate[52]+calculate[55])
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
         elif o_id==19:
             score=10-(calculate[56]+calculate[57])
-            syakudo_value[output[o_id]] = score
+            syakudo_value[output['番号']] = score
 
     return syakudo_value
